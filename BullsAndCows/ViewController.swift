5b3ca45631ad45e0c9d95ff739e9774e9a57d749
PiHan Hsu <pihanhsu@gmail.com>
2016-07-22 14:40:12 +0800

    basic layout and TODOs

--- BullsAndCows/ViewController.swift
+++ BullsAndCows/ViewController.swift
@@ -10,16 +10,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var inputTextField: UITextField!
+    
+    @IBOutlet var inputLabels: [UILabel]!
+    @IBOutlet var resultLabels: [UILabel]!
+    
+    // TODO: 1. decide the data type you want to use to store the answear
+    
+    var index = 0
+
     override func viewDidLoad() {
+        
+        //TODO: 5. update layout with StackView
+        //TODO: 6. Add auto lauout constrant
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setGame()
+     }
+    
+    
+    func setGame() {
+        index = 0
+        
+        for inputLabel in inputLabels {
+            inputLabel.text = ""
+        }
+        for resultLabel in resultLabels {
+            resultLabel.text = ""
+        }
+        
+        generateAnswer()
+        
+    }
+    
+    func generateAnswer() {
+        // TODO: 2. generate your answear here
+        // You need to generate 4 random and non-repeating digits.
+        // Some hints: http://stackoverflow.com/q/24007129/938380
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func enterButtonPressed(sender: AnyObject) {
+        
+        let inputString = inputTextField.text
+        
+        // check if user enter 4 digits
+        guard inputString?.characters.count == 4 else {
+            let alert = UIAlertController(title: "you should input 4 digits to guess!", message: nil, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
+        
+        //show inputLabel
+        inputLabels[index].text = inputTextField.text!
+        index += 1
+        
+        //clear textField
+        inputTextField.text = ""
+        
+        //TODO: 3. check the input number and show the hint on result Label
+        
+        /*
+        resultLabels[index].text = ""
+        */
+        
+        // TODO: 4. update the constant "correct" if the guess is correct
+        
+        //let correct = false
+        
+//        if correct {
+//            let alert = UIAlertController(title: "Wow! You are awesome!", message: nil, preferredStyle: .Alert)
+//            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+//            self.presentViewController(alert, animated: true, completion: nil)
+//        }
+        
     }
+   
+    @IBAction func startANewGame(sender: AnyObject) {
+        setGame()
+    }
+    
+    //Optional TODOs:
+    //1. add a remianing Number
+    //2. add a button and a label to show the answer
+    //3. check there is no duplicate digit in inputNumber
+    //4. check user can't guess the same number twice
 
+    
+    //Advance TODOs:
+    //1. refactor this app with Model
+    //2. refactor this app with Protocol
+    //3. use table View to show the results
+    //4. use your creation to add more fun feature
 
 }
 
